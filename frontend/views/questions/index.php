2dc860b68a150f36de57a54c38dba5dde8886b70
Omerli Liranzo <OJLiranzo@gmail.com>
2021-07-27 12:15:46 -0400

    Update Question Form

--- frontend/views/questions/index.php
+++ frontend/views/questions/index.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="questions-index">
 
-    <h1><?= Html::encode($this->title) ?> <?= Html::a('Create Questions', ['create'], ['class' => 'btn btn-success btn-sm float-right']) ?></h1>
+    <h1><?= Html::encode($this->title) ?> </h1>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
